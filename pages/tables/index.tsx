ca447b8bce0a11c9f2b0e14f9f1c1c55badedbbc
rougenij <rougenij@gmail.com>
2022-05-18 20:52:54 +0300

    Adding AntD modal with Draggable

--- pages/tables/index.tsx
+++ pages/tables/index.tsx
@@ -1,16 +1,43 @@
+import { SetStateAction, useState } from "react";
 import Draggable from "react-draggable";
+import "antd/dist/antd.css";
+import { Modal, Button } from "antd";
 
 const TablesPage = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   const tablesArray = new Array(10);
 
   return (
     <div>
       <Draggable>
-        <div>Table 1</div>
+        <div>
+          <Button type="primary" onClick={showModal}>
+            Table 1
+          </Button>
+          <Modal
+            title="Basic Modal"
+            visible={isModalVisible}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          >
+            <p>This is Table 1 Modal</p>
+          </Modal>
+        </div>
       </Draggable>
     </div>
   );
 };
 
 export default TablesPage;
-        
